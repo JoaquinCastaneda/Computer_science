078036daf5ae6adecaa4a331c8cf020e652b12ed
JoaquinCastaneda <22JUntiveros.Acc@lwetb.ie>
2026-09-15 12:15:45 +0100

    Add files via upload

---
+++ Assignment_1/ComputerScience-Exercise1.py
@@ -0,0 +1,41 @@
+'''
+number1=input("Enter number:")
+number1=float(number1)
+number2=input("Enter another number:")
+number2=float(number2)
+total=number1+number2
+print(total)
+number3=input("Enter a third number:")
+number3=float(number3)
+print(total*number3)
+'''
+'''
+Number1=input("How many slices did you start with:")
+Number1=float(Number1)
+Number2=input("How many slices have you eaten:")
+Number2=float(Number2)
+Total=Number1-Number2
+Total=int(Total)
+print(Total,"slices")
+
+'''
+Name=input("Whats your name:")
+Age=input("How old are you:")
+Age=float(Age)
+total=Age+1
+total=int(total)
+print(f"Hello {Name} next year you will be {total}")
+
+'''
+bill=input("Total price of the bill:")
+bill=float(bill)
+diners=input("How many diners:")
+diners=float(diners)
+total=bill/diners
+print("Each person must pay",total)
+
+Wheight=input("Enter weight in kilograms and convert it pounds:")
+Wheight=float(Wheight)
+total=Wheight*2.204
+print(Wheight,"in pounds is",total)
+'''
